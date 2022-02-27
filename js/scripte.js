2a8c52bffeaf6b318ebae754998a8ba2ce1d1563
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-02-27 23:27:20 +0600

    third

--- js/scripte.js
+++ js/scripte.js
@@ -1,9 +1,30 @@
 const allPlayers = () => {
     const searchValue = document.getElementById('search-box').value;
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
-    console.log(url);
+    // console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data));
-    console.log(searchValue);
+        .then((data) => showAllPlayers(data.player));
+    // console.log(searchValue);
+};
+const showAllPlayers = (players) => {
+    for (const player of players) {
+        const parent = document.getElementById('player-container');
+        const div = document.createElement('div');
+        div.innerHTML = ` <div class="card border p-5">
+    <div class="pro-pic">
+        <img class="w-75" src="${player.strThumb}" alt="">
+    </div>
+    <h3>Name: ${player.strPlayer}</h3>
+    <h4>Country: ${player.strNationality}</h4>
+    <p></p>
+    <div class="allBtn">
+        <button class="btn btn-danger">Delete</button>
+        <button class="btn btn-success">Detials</button>
+    </div>
+</div>`
+        parent.appendChild(div);
+
+    }
+    // console.log(player);
 }
